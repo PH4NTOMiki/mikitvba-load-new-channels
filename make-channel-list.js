630bb63a1fd1de14b851b3cd1cd65429ad0191aa
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-15 17:29:24 +0100

    a

--- make-channel-list.js
+++ make-channel-list.js
@@ -11,22 +11,22 @@ request.get('https://webtvstream.bhtelecom.ba/client/channels', (error, response
 		channels.push(currch);
 		title[currch]=c.title;
 		/*if(i<radioStart){timeshift[currch]=c.timeshift;if(c.cat=="4"){type[currch]="2";} else {type[currch]="1";}} else {type[currch]="3";}*/
-		i<radioStart?(timeshift[currch]=c.timeshift,type[currch]=(c.cat=="4")?"2":"1"):(type[currch]="3")
+		i<radioStart?(timeshift[currch]=Number(c.timeshift),type[currch]=(c.cat==4)?2:1):(type[currch]=3)
 		}
 		let jsonText=JSON.stringify({channels:channels,title:title,timeshift:timeshift,type:type}),
 		variablesText='var channels='+JSON.stringify(channels)+',title='+JSON.stringify(title).replace(/\"([^(\")"]+)\":/g,"$1:")+',timeshift='+JSON.stringify(timeshift).replace(/\"([^(\")"]+)\":/g,"$1:")+',type='+JSON.stringify(type).replace(/\"([^(\")"]+)\":/g,"$1:");
 		// uncomment next line to console.log the result
 		//console.log("result:\n"+jsonText);
-		if(!fs.existsSync('JSON.json')||jsonText!==fs.readFileSync('JSON.json').toString()){
-			fs.writeFile('JSON.json', jsonText, (err, data)=>{
-				if(err)console.log("Unsuccessfully tried to write to JSON.json file, error: ", err);
-				else console.log("Successfully written JSON.json file");
+		if(!fs.existsSync('channels.json')||jsonText!==fs.readFileSync('channels.json').toString()){
+			fs.writeFile('channels.json', jsonText, (err, data)=>{
+				if(err)console.log("Unsuccessfully tried to write to channels.json file, error: ", err);
+				else console.log("Successfully written channels.json file");
 		});
 		}
-		if(!fs.existsSync('variables.js')||variablesText!==fs.readFileSync('variables.js').toString()){
-			fs.writeFile('variables.js', variablesText, (err, data)=>{
-				if(err)console.log("Unsuccessfully tried to write variables.js file, error: ", err);
-				else console.log("Successfully written variables.js file");
+		if(!fs.existsSync('channels.js')||variablesText!==fs.readFileSync('channels.js').toString()){
+			fs.writeFile('channels.js', variablesText, (err, data)=>{
+				if(err)console.log("Unsuccessfully tried to write channels.js file, error: ", err);
+				else console.log("Successfully written channels.js file");
 		});
 		}
 	});
