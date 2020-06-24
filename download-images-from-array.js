df69760c2e8a44d7b32401a3441605f2d97d31f9
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-06-24 13:27:54 +0200

    .

--- download-images-from-array.js
+++ download-images-from-array.js
@@ -8,7 +8,8 @@ i=0,j=fileContent.length;
 function doIt(){if(i>=j)return;
 	request.get(fileContent[i], {encoding: 'binary'}, function(error, response, body) {
 		let a=fileContent[i].split('/').slice(-2);
-		fs.writeFileSync(path.join(__dirname,a[0],a[1]), body, 'binary');
+		if(!fs.existsSync(path.join(__dirname,a[0])))fs.mkdirSync(path.join(__dirname,a[0]));
+		fs.writeFileSync(path.join(__dirname,a[0],'img-'+a[1].split('tint=')[1]+'.jpg'), body, 'binary');
 		i++;
 		doIt();
 	});
