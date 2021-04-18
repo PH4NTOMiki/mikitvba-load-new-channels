895a9deb2d5b4a8dc436bc095e9572fe7c3391d9
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2021-04-18 15:59:18 +0200

    Create walkSync.js

---
+++ walkSync.js
@@ -0,0 +1,11 @@
+const fs = require('fs'), path = require('path');
+const walkSync = (dir, filelist = []) => {
+    fs.readdirSync(dir).forEach(file => {
+  
+      filelist = fs.statSync(path.join(dir, file)).isDirectory()
+        ? walkSync(path.join(dir, file), filelist)
+        : filelist.concat(path.join(dir, file));
+  
+    });
+  return filelist;
+  }
